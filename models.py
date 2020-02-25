17d9246842c700cfedba15357b2fe6e87b6536c4
babaprincedaouda71 <61049170+babaprincedaouda71@users.noreply.github.com>
2020-02-25 22:53:38 +0100

    Update models.py

--- models.py
+++ models.py
@@ -7,66 +7,96 @@ from datetime import datetime
 
 
 class individu(models.Model):
-    code = models.CharField(max_length = 5, primary_key=True, default="")
+    cin = models.CharField(max_length = 8, primary_key = True, null = False)
     nom = models.CharField(max_length = 100, default="")
     prenoms = models.CharField(max_length = 42, default="")
-    photo = models.ImageField(default="")
+    #photo = models.ImageField(default="")
     mail = models.EmailField(max_length = 30, default='exemple@gmail.com')
     tel = models.CharField(max_length = 15, default="")
     addr = models.CharField(max_length = 60, default="")
     ville_residence = models.CharField(max_length = 250, default="")
     username = models.CharField(max_length = 30, default="")
     password = models.CharField(max_length = 15, default="")
-    etat_civil = models.CharField(max_length = 15, default="")
     genre_choice = (
+        ('Masculin', 'Masculin'),
+        ('Feminin', 'Feminin'),
+    )
+    genre = models.CharField(max_length = 15, choices = genre_choice)
+    civil_choice = (
         ('Celibataire', 'Celibataire'),
         ('Marié', 'Marié'),
         ('Veuf/Veuve', 'Veuf/Veuve'),
     )
-    genre = models.CharField(max_length = 20, choices = genre_choice)
+    etat_civil = models.CharField(max_length = 15, choices = civil_choice)
     date_nais = models.DateField(default=datetime.now)
     est_admin = models.BooleanField(default=False)
 
-"""class candidat(models.Model):
-    num = models.CharField(max_length = )
-    date
-    fonction"""
+class candidat(models.Model):
+    num_insc = models.AutoField(primary_key = True)
+    date = models.DateTimeField()
+    fonction = models.CharField(max_length = 60)
+    entreprise = models.CharField(max_length = 60)
 
 
 """class diplome(models.Model):
-code_dip = models.CharField(max_length=100, default="")
+diplome_choice = (
+    ('mst001', 'mst001'),
+    ('mst002', 'mst002'),
+    ('ing001', 'ing001'),
+)
+code_dip = models.CharField(max_length=20, choices = diplome_choice, primary_key = True)
 type_dip = models.CharField(max_length=100, default="")
 date_dip = models.DateTimeField()
 libelle_dip = models.CharField(max_length=100, default="")
 
 
 class these(models.Model):
-id_these = models.CharField(max_length=100, default="")
-titre_these = models.CharField(max_length=100, default="")
+    id_these = models.Auto.Field(max_length=100, primary_key = True)
+    titre_these = models.CharField(max_length=100, default="")
 
 
 class labo(models.Model):
-id_labo = models.CharField(max_length=100, default="")
-libelle_labo = models.CharField(max_length=100, default="")
-id_equipe = models.ForeignKey(
+    labo_choice = (
+        ('', ''),
+        ('', ''),
+        (),
+        (),
+    )
+    id_labo = models.CharField(max_length=100, primary_key = True, choices = labo_choice)
+    libelle_labo = models.CharField(max_length=100, default="")
+    id_equipe = models.ForeignKey(
         equipe_rechere, on_delete=models.CASCADE)
 
 
 class enregistrement(models.Model):
-id_enregistrement = models.CharField(max_length=100, default="")
-rapport = models.CharField(max_length=100, default="")
-articles = models.CharField(max_length=100, default="")
-date_enregristrement = models.DateTimeField()
+    id_enregistrement = models.AutoField(primary_key = True)
+    rapport = models.CharField(max_length=100, default="")
+    articles = models.CharField(max_length=100, default="")
+    date_enregristrement = models.DateTimeField()
 
 
 class equipe_rechere(models.Model):
-id_equipe = models.CharField(max_length=100, default="")
-libelle_equipe = models.CharField(max_length=100, default="")
-pro_id= models.ForeignKey(
+    id_equipe = models.AutoField(primary_key = True)
+    libelle_equipe = models.CharField(max_length=100, default="")
+    pro_id= models.ForeignKey(
         Professeur, on_delete=models.CASCADE)
-id_docorant = models.ForeignKey(
-        doctorant on_delete=models.CASCADE)  
-      
+    id_docorant = models.ForeignKey(
+        doctorant on_delete=models.CASCADE)
 """
 
-class posseder_dip
+class doctorant(model.Model):
+    id_doctorant = models.AutoField(primary_key = True)
+    date_dip = model.DateTimeField()
+
+
+class posseder_dip(models.Model):
+    code_dip = model.ForeignKey(diplome, on_delete = models.CASCADE)
+    id_doctorant = model.ForeignKey(doctorant, on_delete = models.CASCADE)
+
+class professeur(model.Model):
+    code_prof = models.AutoField(primary_key = True)
+    status = models.BooleanField(default = False)
+
+
+class admin(model.Model):
+    id_admin = models.AutoField(primary_key = True)
